4a8e96277f89e1714ca8467ce94a907b81043e69
kpuhger <krpuhger@gmail.com>
2023-02-12 08:47:26 -0800

    set up perturbation functions

--- graph_neuralmorpho/perturbations.py
+++ graph_neuralmorpho/perturbations.py
@@ -1,22 +1,40 @@
 """Perturbations of neuron structures for contrastive learning."""
-import numpy as np
 import pandas as pd
 
 
-def perturbation_point(neuron: pd.DataFrame, proportion: float = 0.5) -> pd.DataFrame:
+def perturb_points(neuron_swc: pd.DataFrame, prop: float = 0.5) -> pd.DataFrame:
+    """Perturb a neuron by shifting a prop of points by a random distance (0-10 µm).
 
-    # select proportion of points to perturb
-    print(proportion)
-    np.random.randint(0, 11)  # random distance between 0 and 10 µm
+    Args:
+        neuron_swc (pd.DataFrame): swc data.
+        prop (float, optional): Proportion of points to shift. Defaults to 0.5.
 
-    # shift all points by distance
+    Returns:
+        pd.DataFrame: Neuron swc data with perturbed points.
+    """
+    # TODO:
+    # 1. select prop of points to perturb
+    # 2. set random distance between 0 and 10 µm
+    # 3. shift all points by distance
 
-    return neuron
-
-
-def perturbation_branch(neuron: pd.DataFrame) -> pd.DataFrame:
     raise NotImplementedError
 
 
-def perturbation_rotate(neuron: pd.DataFrame) -> pd.DataFrame:
+def drop_branches(neuron_swc: pd.DataFrame) -> pd.DataFrame:
+    """Drop a random branch from a neuron."""
+    # TODO:
+    # 1. implement probability of branch selection from ZhaoEtAl2022
+    # 2. select 2% of branches to drop based on probability from 1.
+    # 3. drop branches from neuron
+
+    raise NotImplementedError
+
+
+def rotate_neuron(neuron_swc: pd.DataFrame) -> pd.DataFrame:
+    """Rotate a neuron about an axis by a random angle (0-360 degrees)."""
+    # TODO:
+    # 1. select random axis to rotate about
+    # 2. select random angle between 0 and 360 degrees
+    # 3. rotate neuron about axis by angle
+
     raise NotImplementedError
